52642e4a618645844828f0096291320a39ab460a
Otavio Polli <otavio.polli.semil@gmail.com>
2026-07-21 12:53:07 -0300

    Aula 45: Desafio

---
+++ exercicios_1/src/fundamentos/desafioAritimetico.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class desafioAritimetico {
+	public static void main(String[] args) {
+		
+		double prt1 = 6 * (3+2);
+		double prt2 = Math.pow(prt1, 2);
+		double prt3 = prt2 / (3*2);
+		
+		double seg1 = (1-5) * (2-7);
+		double seg2 = seg1 / 2;
+		double seg3 = Math.pow(seg2, 2);
+		
+
+		double valorE = prt3 - seg3;
+		double valorF = Math.pow(valorE, 3);
+		double valorFinal = valorF / (Math.pow(10, 3));
+		
+		System.out.println((int) valorFinal);
+		
+	}
+}
