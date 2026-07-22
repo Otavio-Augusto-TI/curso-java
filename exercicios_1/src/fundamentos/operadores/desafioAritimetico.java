37b2b90774ab854001b650e8d7389bda45b09ee1
Otavio <otavioapolli0912@gmail.com>
2026-07-22 13:14:16 -0300

    Ajustando arquivos

--- exercicios_1/src/fundamentos/operadores/desafioAritimetico.java
+++ exercicios_1/src/fundamentos/operadores/desafioAritimetico.java
@@ -1,4 +1,4 @@
-package fundamentos;
+package fundamentos.operadores;
 
 public class desafioAritimetico {
 	public static void main(String[] args) {
--- exercicios_1/src/fundamentos/operadores/logicos.java
+++ exercicios_1/src/fundamentos/operadores/logicos.java
@@ -16,12 +16,12 @@ public class logicos {
 		System.out.println("\ntabela verdade E - &&");
 		System.out.println(true && true);
 		System.out.println(true && false);
-		System.out.println(false && true);
-		System.out.println(false && false);
+	//	System.out.println(false && true);
+	//	System.out.println(false && false);
 		
 		System.out.println("\ntabela verdade OU - ||");
-		System.out.println(true || true);
-		System.out.println(true || false);
+	//	System.out.println(true || true);
+	// System.out.println(true || false);
 		System.out.println(false || true);
 		System.out.println(false || false);
 		
