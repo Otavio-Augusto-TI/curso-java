2fa4587d8f52de2812ece1862430db0b78260340
Otavio <otavioapolli0912@gmail.com>
2026-07-23 00:37:03 -0300

    Aula 52: Operador Unario

---
+++ exercicios_1/src/fundamentos/operadores/unarios.java
@@ -0,0 +1,26 @@
+package fundamentos.operadores;
+
+public class unarios {
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		//pos fixada
+		a++; // a = a + 1
+		a--; // a = a - 1
+		
+		// pre fixada
+		++b; // b = b + 1
+		--b; // b = b - 1
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		System.out.println("DESAFIO");
+		System.out.println(++a == b--);
+		System.out.println(a == b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
