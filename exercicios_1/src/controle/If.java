ce0080a2b38d2c1f3254e34064d2311feaeda237
Otavio <otavioapolli0912@gmail.com>
2026-07-28 12:19:12 -0300

    Aula 63: desafio If

--- exercicios_1/src/controle/If.java
+++ exercicios_1/src/controle/If.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class If {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Informe a média: ");
 		double media = input.nextDouble();
-		
+
+		// NÃO USAR ; em estruturas de controle, uma excessão
 		if (media <= 10 && media >= 7.0) {
 			System.out.println("APROVADO");
-		} else if (media >= 0 && media < 7 ) { 
+		} else if (media >= 0 && media < 7) {
 			System.out.println("REPROVADO");
 		} else {
 			System.out.println("Valor inválido");
 		}
-		
+
 		input.close();
-		
-	} 
+
+	}
 }
