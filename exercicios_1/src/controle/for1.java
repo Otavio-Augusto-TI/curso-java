7fc4690623250bba36638fc5158cc2290854b705
Otavio <otavioapolli0912@gmail.com>
2026-07-30 21:27:47 -0300

    Aula 71: For 1

---
+++ exercicios_1/src/controle/for1.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class for1 {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 20; i++) {
+			System.out.printf("i = %d\n", i);
+		}
+			
+		
+	// Laço infinito
+	//	for(;true;) {
+	//		System.out.println("FIM!");
+	//	}
+		
+	}
+}
