703327ef84c50e0390ada919e788ee77b931c00e
Otavio <otavioapolli0912@gmail.com>
2026-07-31 14:01:28 -0300

    Aula 76: For 2

---
+++ exercicios_1/src/controle/for2.java
@@ -0,0 +1,13 @@
+package controle;
+
+// Desafio: fazer uma contagem regressiva de 10 a 0 pulando de 2 em 2
+
+public class for2 {
+	public static void main(String[] args) {
+			
+		for (int c = 10; c >= 0; c -= 2) {
+			System.out.println(c);
+		}
+		
+	}
+}
