1a05aeac1c17a7959025e28e16f63faeceda3c5e
Otavio <otavioapolli0912@gmail.com>
2026-07-30 21:18:50 -0300

    Aula 70: While

---
+++ exercicios_1/src/controle/whileDeterminado.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class whileDeterminado {
+	public static void main(String[] args) {
+		
+		int contador = 1;
+		
+		while (contador <= 50) {
+			System.out.printf("i = %d\n", contador);
+			contador += 2;
+		}
+		
+	}
+}
