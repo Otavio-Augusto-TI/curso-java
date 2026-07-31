e67a06bbc1fe9074d9576966b97af4cf7b503a44
Otavio <otavioapolli0912@gmail.com>
2026-07-31 14:16:11 -0300

    Aula 77: For 3 = Ordem de variaveis, e criando mais de um laço dentro de outro

---
+++ exercicios_1/src/controle/for3.java
@@ -0,0 +1,23 @@
+package controle;
+
+public class for3 {
+	public static void main(String[] args) {
+		
+//		int i = 5;
+//		
+//		System.out.println("antes do laço:" + i);
+//		
+//		for (i = 0; i < 10; i++) {
+//			System.out.println(i);
+//		}
+//		
+//		System.out.println(i + "fora do laco");
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("[%d, %d] \n", i, j);
+			}
+		}
+		
+	}
+}
