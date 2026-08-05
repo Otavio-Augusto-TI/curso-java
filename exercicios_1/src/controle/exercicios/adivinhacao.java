009c99e05e493d9ff4895f9de1f393c2dff4aa99
Otavio Polli <otavio.polli.semil@gmail.com>
2026-08-05 12:32:04 -0300

    Exercicio adivnhação correto

--- exercicios_1/src/controle/exercicios/adivinhacao.java
+++ exercicios_1/src/controle/exercicios/adivinhacao.java
@@ -4,31 +4,40 @@ import java.util.Scanner;
 
 public class adivinhacao {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Jogo de adinhação");
-		
+
 		int max = 10;
 		int min = 1;
 		int range = max - min + 1;
-		
-		for (int i = 0; i <= 10; i++) {
-			int rand = (int)(Math.random() * range) + min;
-			
+
+		int rand = (int) (Math.random() * range) + min;
+
+		for (int i = 10; i >= 0; i--) {
+
 			System.out.print("Digite seu palpite: ");
 			int palpite = input.nextInt();
-			
-			System.out.println(rand);
+
+			if (palpite > rand) {
+				System.out.println(palpite + " é maior");
+				System.out.println("Tentativas: " + i);
+			}
+
+			if (palpite < rand) {
+				System.out.println(palpite + " é menor");
+				System.out.println("Tentativas: " + i);
+			}
+
 			if (palpite == rand) {
 				System.out.println("Parabéns, você acertou!!");
+				System.out.println("Seu número : " + palpite);
 				break;
-			} else {
-				System.out.printf("Erro, você tem mais %d tentativas \n", 10 - i );
 			}
-						
 		}
-		
+
+		System.out.println("Numero: " + rand);
 		System.out.println("Fim de jogo.");
 		input.close();
 	}
