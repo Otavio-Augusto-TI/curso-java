809cbb05f6208441f1b2734ce89e20e96184dd4f
Otavio <otavioapolli0912@gmail.com>
2026-08-13 22:00:45 -0300

    Aula 100 Desafio metodo

--- exercicios_1/src/classes/data.java
+++ exercicios_1/src/classes/data.java
@@ -7,4 +7,16 @@ public class data {
 	int mes;
 	int ano;
 	
+	String obterDataForm() {
+		String diaConv = Integer.toString(dia);
+		String mesConv = Integer.toString(mes);
+		String anoConv = Integer.toString(ano);
+		
+		return diaConv + "/" + mesConv + "/" + anoConv;
+		
+		// OU
+		
+		// return String.format("%d/%d/%d", dia, mes, ano);
+	}
+	
 }
--- exercicios_1/src/classes/dataTeste.java
+++ exercicios_1/src/classes/dataTeste.java
@@ -7,20 +7,22 @@ public class dataTeste {
 		
 		data date1 = new data();
 		
-		date1.dia = 9;
-		date1.mes = 12;
+		date1.dia = 19;
+		date1.mes = 8;
 		date1.ano = 2008;
 		
-		System.out.printf("%d/%d/%d", date1.dia, date1.mes, date1.ano);
-		System.out.println();
+		//System.out.printf("%d/%d/%d", date1.dia, date1.mes, date1.ano);
+		//System.out.println();
 		
 		data date2 = new data();
 		
 		date2.dia = 9;
 		date2.mes = 12;
-		date2.ano = 2026;
+		date2.ano = 1980;
 	
-		System.out.printf("%d/%d/%d", date2.dia, date2.mes, date2.ano);
+		System.out.println(date1.obterDataForm());
+		System.out.println(date2.obterDataForm());
+		// System.out.printf("%d/%d/%d", date2.dia, date2.mes, date2.ano);
 
 	}
 	
