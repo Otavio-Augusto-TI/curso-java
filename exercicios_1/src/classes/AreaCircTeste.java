b6c5768d07439555b5bb39f302bdfc148cfc6525
Otavio <otavioapolli0912@gmail.com>
2026-08-16 22:17:23 -0300

    Aula 106: Membros de Classe vs Instancia

---
+++ exercicios_1/src/classes/AreaCircTeste.java
@@ -0,0 +1,20 @@
+package classes;
+
+public class AreaCircTeste {
+	public static void main(String[] args) {
+		
+		AreaCirc a1 = new AreaCirc(10);
+		// a1.pi = 5;
+
+		AreaCirc a2 = new AreaCirc(5);
+		// a2.pi = 10;
+
+		// AreaCirc.pi = 3.1415;
+		
+		System.out.println(a1.area());
+		System.out.println(a2.area());	
+		System.out.println(AreaCirc.PI);
+		System.out.println(Math.PI);
+		System.out.println(AreaCirc.area(15));
+	}
+}
