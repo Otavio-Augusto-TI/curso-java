ca47e4280c5253dd7798c93c3259dd979d1751da
Otavio <otavioapolli0912@gmail.com>
2026-08-18 22:08:34 -0300

    Aula 111: Primeiro Trauma

---
+++ exercicios_1/src/classes/PrimeiroTrauma.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class PrimeiroTrauma {
+
+	int a = 3; // não pode mexer
+	static int b = 4;
+
+	public static void main(String[] args) {
+		
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		
+		System.out.println(p.a);
+		
+		System.out.println(b);
+		
+	}
+}
